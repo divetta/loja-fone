90fbfe742afdba754b5e6b601770f53a94adee22
Raphael Divetta Bronzo <divetta@gmail.com>
2018-12-18 00:53:29 +0000

    [013] - Desafio Avançado

--- js/button-store.js
+++ js/button-store.js
@@ -3,5 +3,12 @@ const $buttonAddTocart = window.document.querySelector(".-second");
 $buttonAddTocart.addEventListener("click", handleClick);
 
 function handleClick() {
-  console.log("aew");
+  $cartCounter = window.document.querySelector("a.-last");
+
+  let existingQty = $cartCounter.text;
+  let newQty = existingQty.replace(/(\d+)+/g, function(match, number) {
+    return parseInt(number) + 1;
+  });
+
+  $cartCounter.text = newQty;
 }
