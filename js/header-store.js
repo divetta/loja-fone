d5123287ef254eb86f52eb82d54b2be228d372f2
Raphael Divetta Bronzo <divetta@gmail.com>
2019-01-03 19:09:52 -0200

    [017] - Desafio Intermediário

--- js/header-store.js
+++ js/header-store.js
@@ -1,5 +1,12 @@
+const $magnifier = document.querySelector(".magnifier");
 const $search = document.querySelector(".search");
+const $close = document.querySelector(".close");
 
-$search.addEventListener("click", () => {
+$magnifier.addEventListener("click", () => handleClickSearch());
+$close.addEventListener("click", () => handleClickSearch());
+
+function handleClickSearch() {
   $search.classList.toggle("-active");
-});
+  $close.classList.toggle("-active");
+  $magnifier.classList.toggle("-active");
+}
