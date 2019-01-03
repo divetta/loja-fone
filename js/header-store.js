3a7afa0f5434fa0c732fe7ecb5ba850d977fc9c3
Raphael Divetta Bronzo <divetta@gmail.com>
2019-01-03 21:31:49 +0000

    Revert "[017] - Desafio Avançado"
    This reverts commit af7fb0bbe502831bf96d143e87d9f160d424dfdf.

--- js/header-store.js
+++ js/header-store.js
@@ -1,12 +1,6 @@
 const $magnifier = document.querySelector(".magnifier");
-const $search = document.querySelector(".search");
-const $close = document.querySelector(".close");
 
-$magnifier.addEventListener("click", () => handleClickSearch());
-$close.addEventListener("click", () => handleClickSearch());
-
-function handleClickSearch() {
+$magnifier.addEventListener("click", () => {
+  const $search = document.querySelector(".search");
   $search.classList.toggle("-active");
-  $close.classList.toggle("-active");
-  $magnifier.classList.toggle("-active");
-}
+});
